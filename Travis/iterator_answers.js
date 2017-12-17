124dd52f98e7c81f5a810244982ffa9a3391a637
Travis1282 <tmail20@gmail.com>
2017-12-17 16:39:42 -0600

    Commit 9 - map done

--- Travis/iterator_answers.js
+++ Travis/iterator_answers.js
@@ -3,8 +3,47 @@
 // Using the array words, log each word in words in upper case using .forEach.
 const words = ["Ho", "Chi", "Minh", "City", "was", "once", "known", "as", "Prey", "Nokor"].forEach(logElements);
 
-
 function logElements(value) {
     console.log(`${value}`);
 }
-// new Map([['foo', 3], ['bar', {}], ['baz', undefined]]).forEach(logElements);
+
+
+// .map is like .forEach but it returns a new array with the selections and / or transformations applied in the iterator. .map saves to a new array and does not alter the original array.
+
+// Map to a new array that stores only the first letter from each word in more_words.
+const more_words = ["Joe", "Overeats", "Eggs"];
+// Expected result:
+
+// ["J", "O", "E"]
+
+
+
+more_Letters = more_words.map((letters)=>{
+	return letters.split('')
+});
+
+
+// let numBlers =[2,4,5,6,9,10]
+
+// const Odds = numBlers.filter((num)=>{
+// 	return num % 2==1
+// }).map((num)=>{
+// 	return 2 * 10;
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
